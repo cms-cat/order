b7dc6c608879e61f8f915bc711a4e7bd25680625
Davide Valsecchi <valsecchi.davide94@gmail.com>
2023-09-27 14:48:17 +0200

    scheleton for models

---
+++ order/models/campaign.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+__all__ = ["Campaign"]
+
+import os
+import glob
+from typing import Any
+from pydantic import BaseModel
+
+class Campaign(BaseModel):
+
+    def __init__(self):
+        
+        pass
+
+
+
---
+++ order/models/dataset.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+__all__ = ["Dataset"]
+
+import os
+import glob
+from typing import Any
+from pydantic import BaseModel
+
+class Dataset(BaseModel):
+
+    def __init__(self):
+        
+        pass
+
+
+
